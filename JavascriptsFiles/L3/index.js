45c53a1a05183c473bc07960f668e4f4630dcace
Nitin <nitindharmesh01@gmail.com>
2026-06-24 16:32:16 +0530

    Loops

---
+++ JavascriptsFiles/L3/index.js
@@ -0,0 +1,8 @@
+// While Loop: Repeat some code While some condition is true;
+let username = "";
+while (username === ""){
+    username = window.prompt("UserName:");
+}
+console.log(username);
+
+// 2:30
