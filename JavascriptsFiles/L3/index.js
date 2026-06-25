58a28d93fb8020a548ea84e012ec1de377e5f625
Nitin <nitindharmesh01@gmail.com>
2026-06-25 07:05:42 +0530

    Loops for loops

--- JavascriptsFiles/L3/index.js
+++ JavascriptsFiles/L3/index.js
@@ -1,8 +1,31 @@
 // While Loop: Repeat some code While some condition is true;
-let username = "";
-while (username === ""){
-    username = window.prompt("UserName:");
-}
-console.log(username);
+// let username = "";
+// while (username === ""){
+//     username = window.prompt("UserName:");
+// }
+// console.log(username);
 
-// 2:30
+// 2:30: do Wwhile Loop;
+
+// let loggedin = false;
+// let usernname;
+// let password;
+
+// while (!loggedin) {
+//     usernname = window.prompt("Enter your username")
+//     password = window.prompt("Enter your password") 
+    
+//     if (usernname === "myUsername" && password=== "myPassword") {
+//         loggedin = true;
+//         console.log("You are logged in!");
+//     }
+//     else{
+//         console.log("Invalid Credential Please try again!");
+//     }
+// }
+
+// For Loops: repeat some code a LIMITED amount of time;
+
+for(let i = 2; i <= 10; i+= 2){
+    console.log(i);   
+}
