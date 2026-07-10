9fcf81041f71a71b4dd93b68f29b547a94f88ea5
Nitin <nitindharmesh01@gmail.com>
2026-07-10 06:51:57 +0530

    Objects

---
+++ JavascriptsFiles/objects/index.js
@@ -0,0 +1,19 @@
+// Object = " A collection of related properties / methods";
+// object =  key:value, funciton();
+
+// Methods are functions that belongs to an object;
+
+const people = {
+    firstName:"Nitin",
+    lastName:"Dharmesh",
+    age:23,
+    isEmployed: true,
+    sayHello:()=>{console.log("Hey there i am an function");
+    }
+}
+console.log(people.firstName);
+console.log(people.lastName);
+console.log(people.age);
+console.log(people.sayHello());
+
+
