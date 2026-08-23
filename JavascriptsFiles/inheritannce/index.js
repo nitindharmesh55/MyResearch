a9033c3130e2fc41e523650ff12ed75730f97dca
Nitin <nitindharmesh01@gmail.com>
2026-08-23 15:56:44 +0530

    Classess and super Keywords

--- JavascriptsFiles/inheritannce/index.js
+++ JavascriptsFiles/inheritannce/index.js
@@ -1,85 +1,119 @@
-// Inherritance: allows a new class to inherit properties and methods from the existing class;
-// (parent to child)
+// Inheritance: Allow a new class to inherit properties and mathod from an existing class;
 
 
 class Animal{
     alive = true;
-
     eat(){
-        console.log(`${this.name} is eating`);
-        return;
+        console.log(`This ${this.name} is eating`);
+        
     }
-
     sleep(){
-        console.log(`This ${this.name} is sleeping`);
+        console.log(`This ${this.name} is sleep`);
+        
     }
-
 }
 
-class Rabbit extends Animal{
-    name = "Rabbit";
+
+class Rabbit extends Animal {
+ name = "Rabbit";
+
+ run(){
+    console.log(`This ${this.name} can Run`);
+    
+ }
 }
 
-class Fish extends Animal{
+class fish extends Animal {
     name = "Fish";
+
+    swim(){
+        console.log(`This ${this.name} is swiming`);
+        
+    }
 }
-class hawk extends Animal{
+
+class Hawk extends Animal{
     name = "Hawk";
+
+    fly(){
+        console.log(`This ${this.name} is flying`);
+        
+    }
 }
 
 
 const rabbit = new Rabbit();
-const fish = new Fish();
-const Hawk = new hawk();
-
-
+const Fish = new fish();
+const hawk = new Hawk();
 
 rabbit.eat();
+rabbit.alive;
+rabbit.sleep();
+
+rabbit.run();
+Fish.swim();
+hawk.fly();
 
 
 
+// Super Keywords: used in classes to call the constructor or access the properties and methods of parent;
+// This - this object;
+// Super - this parent;
 
 
-// Super: keyword is used in classes to call the constructor or acess the properties and methods of a parent(Superclasses);
-
-// this =  this Object;
-// super the parent
-
-
-class School{
-    constructor(name){
+class Cars {
+    constructor(name, speed){
         this.name = name;
+        this.speed = speed;
 
     }
-
-}
-
-class staff extends School{
-   
-    constructor(name, role){
-         super(name);
+    move(speed){
+        console.log(`This ${this.name} speed of ${speed}km/h`);
         
-        this.role = role;
-
-    }
-
-}
-
-class students extends School{
- constructor(name, subject){
-    super(name);
-       
-        this.role = subject;
-
     }
 }
 
 
-const stff = new staff("Ritki", "Professor");
+class BMW extends Cars {
+    constructor(name, speed, engine){ 
+        super(name, speed);
+        this.engine = engine;
+    }
 
-const student = new students("Nitin", "Mathematics");
+    run(){
+        console.log("It can Runn");;
+        super.move(this.engine)
+        
+    }
+   
+}
+
+class Lembo extends Cars{
+     constructor(name, speed, engine){
+        super(name, speed);
+        this.engine = engine;
+    }
+    
+}
+
+class Cooper extends Cars{
+     constructor(name, speed, engine){
+        super(name, speed);
+        this.engine = engine;
+    }
+
+}
+
+const V2 =  new BMW("V2", 25, 255);
+const V3 =  new Lembo("B2", 25, "Inline");
+
+console.log(V2.name);
+console.log(V2.speed);
+console.log(V2.engine);
+
+V2.run();
 
 
+// 5:48:00 super Keyword;
 
-// 5.:46;
 
