b9cb6709de9375bfcd8a87de1eb9461ce34eddb4
Nitin <nitindharmesh01@gmail.com>
2026-09-03 23:49:22 +0530

    Student Management Systems

---
+++ JavascriptsFiles/Programm/Student Managment System/script.js
@@ -0,0 +1,5 @@
+let studentName = document.getElementById("studentName");
+let age  =  document.getElementById("age");
+let marks = document.getElementById("marks");
+let submit =  document.getElementById("Submit");
+let outPut = document.getElementById("output");
